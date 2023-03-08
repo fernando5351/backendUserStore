920d006faf8a2585885c76e30f7d676d0cdcf61c
schiffer02 <isaacfernando5351@gmail.com>
2023-03-07 18:59:11 -0600

    endpoint shoping get y post

--- src/schemas/shopingSchema.js
+++ src/schemas/shopingSchema.js
@@ -2,20 +2,19 @@ const Joi = require("joi");
 
 const id = Joi.number().integer();
 const idPastel = Joi.number();
-const idFlavor = Joi.number();
+const idFlavor1 = Joi.number();
+const idFlavor2 = Joi.number();
 const idDecoration = Joi.number();
-const idSize = Joi.number();
 const idUser = Joi.number();
 const cant  = Joi.number();
-const estado = Joi.number();
 const idColor1 = Joi.number();
 const idColor2 = Joi.number();
 
 const createShoppingSchema = Joi.object({
     idPastel: idPastel.required(),
-    idFlavor: idFlavor.required(),
+    idFlavor1: idFlavor1.required(),
+    idFlavor2: idFlavor2.required(),
     idDecoration: idDecoration.required(),
-    idSize: idSize.required(),
     idUser: idUser.required(),
     idColor1: idColor1.required(),
     idColor2: idColor2.required()
--- src/services/shoppingService.js
+++ src/services/shoppingService.js
@@ -6,8 +6,8 @@ class Shopping{
     constructor(){}
 
     async create(data){
-        const { idPastel, idFlavor, idDecoration, idSize, idUser, idColor1, idColor2 } = data;
-        const query = `INSERT INTO pedidos (idPastel, idFlavor, idDecoration, idSize, idUser, estado, idColor1, idColor2) VALUES ("${idPastel}", "${idFlavor}", "${idDecoration}", "${idSize}", "${idUser}", "1","${idColor1}","${idColor2}");`;
+        const { idPastel, idFlavor1, idFlavor2, idDecoration, idUser, idColor1, idColor2 } = data;
+        const query = `INSERT INTO pedidos (idPastel, idFlavor1, idFlavor2, idDecoration, idUser, estado, idColor1, idColor2) VALUES ("${idPastel}", "${idFlavor1}", "${idFlavor2}", "${idDecoration}", "${idUser}", "1","${idColor1}","${idColor2}");`;
         try {
             const res = await factory(query);
             return res;
@@ -21,21 +21,20 @@ class Shopping{
         SELECT color1.nameColor AS color1,
         color2.nameColor AS color2,
         pastel.namePastel,
-        flavor.nameFlavor AS flavor_name,
+        flavor1.nameFlavor AS flavor1,
+        flavor2.nameFlavor AS flavor2,
         decoration.nameDecoration AS decoration_name,
-        size.nameSize,
-        size.sizeDescription,
         pastel.imgurl,
+        pastel.price,
         pedidos.cant
         FROM pedidos
         INNER JOIN color AS color1 ON pedidos.idColor1 = color1.id
         INNER JOIN color AS color2 ON pedidos.idColor2 = color2.id
-        INNER JOIN flavor ON pedidos.idFlavor = flavor.id
+        INNER JOIN flavor AS flavor1 ON pedidos.idFlavor1 = flavor1.id
+        INNER JOIN flavor AS flavor2 ON pedidos.idFlavor2 = flavor2.id
         INNER JOIN decoration ON pedidos.idDecoration = decoration.id
-        INNER JOIN size ON pedidos.idSize = size.id
         INNER JOIN pastel ON pedidos.idPastel = pastel.id
-        WHERE pedidos.idUser = ${id} AND pedidos.estado = 1;
-        `;
+        WHERE pedidos.idUser = ${id} AND pedidos.estado = 1;`;
         const res = await factory(query);
         try {
             return res
@@ -47,3 +46,25 @@ class Shopping{
 }
 
 module.exports = Shopping;
+
+
+
+        // SELECT color1.nameColor AS color1,
+        // color2.nameColor AS color2,
+        // flavor.namePastel,
+        // flavor.nameFlavor AS flavor_name1,
+        // flavor.nameFlavor AS flavor_name2,
+        // decoration.nameDecoration AS decoration_name,
+        // size.nameSize,
+        // size.sizeDescription,
+        // pastel.price,
+        // pedidos.cant
+        // FROM pedidos
+        // INNER JOIN color AS color1 ON pedidos.idColor1 = color1.id
+        // INNER JOIN color AS color2 ON pedidos.idColor2 = color2.id
+        // INNER JOIN flavor ON pedidos.idFlavor1 = flavor.id
+        // INNER JOIN flavor ON pedidos.idFlavor2 = flavor.id
+        // INNER JOIN decoration ON pedidos.idDecoration = decoration.id
+        // INNER JOIN size ON pedidos.idSize = size.id
+        // INNER JOIN pastel ON pedidos.idPastel = pastel.id
+        // WHERE pedidos.idUser = 10 AND pedidos.estado = 1;
